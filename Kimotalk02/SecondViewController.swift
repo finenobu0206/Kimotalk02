9be5ae45e89a1dbead03df92e5a37c1563433b57
finenobu0206 <finenobu0206@gmail.com>
2019-11-03 23:13:17 +0900

    Initial Commit

---
+++ Kimotalk02/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Kimotalk02
+//
+//  Created by nobuharu-imaoka on 2019/11/03.
+//  Copyright © 2019 nobuharu-imaoka. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
